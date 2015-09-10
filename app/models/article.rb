993970400de0578d1cdf5dd34bb81538854070a2
Travis Haby <travishaby@gmail.com>
2015-09-10 14:23:21 -0600

    first commit

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ActiveRecord::Base
+  before_save :set_slug
+
+  private
+
+    def set_slug
+      self.slug = title.parameterize
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create(title: "Why VIM sucks", body: "VIM is the worst editor in the world. It's truly baffling that people use it as a way to program. When will they wake up and realize that Atom is so much more superior?")
+Article.create(title: "Torchy's Tacos is the best", body: "Torchy's Tacos is the most delicious taco chain in the world. I can't wait 'til they come to Denver and I can feast upon as many delicous tacos as I want.")
+Article.create(title: "Miracle", body: "Great moments... are born from great opportunity. And that's what you have here, tonight, boys. That's what you've earned here tonight. One game. If we played 'em ten times, they might win nine. But not this game. Not tonight. Tonight, we skate with them. Tonight, we stay with them. And we shut them down because we can! Tonight, WE are the greatest hockey team in the world. You were born to be hockey players. Every one of you. And you were meant to be here tonight. This is your time. Their time is done. It's over. I'm sick and tired of hearing about what a great hockey team the Soviets have. Screw 'em. This is your time. Now go out there and take it.")
+Article.create(title: "Perseverance", body: "If something is important enough, even if the odds are against you, you should still do it.")
